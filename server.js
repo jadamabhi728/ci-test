9455ad79ff9f5373f62e87ae87512a6f95e1252a
Aakash piplodiya <aakash@intivahealth.com>
2026-02-21 15:30:20 +0530

    add ci workflow correctly

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log('hello world');
+});
+
